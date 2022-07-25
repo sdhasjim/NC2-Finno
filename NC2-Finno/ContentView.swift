5efdeeeabf500ea2a8b4b3194c3f7b89de52e46b
Samuel Dennis <sdhasjim@gmail.com>
2022-07-25 10:03:51 +0700

    Initial Commit

---
+++ NC2-Finno/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NC2-Finno
+//
+//  Created by Samuel Dennis on 25/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NC2-Finno/NC2_FinnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_FinnoApp.swift
+//  NC2-Finno
+//
+//  Created by Samuel Dennis on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NC2_FinnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
